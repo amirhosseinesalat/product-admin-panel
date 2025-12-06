b3544bc93c144e0b93a6339a847ec24fc5fc5634
amirhossein esalatfar <sltamirhosein@gmail.com>
2025-12-06 11:36:18 +0330

    added tableProducts with style

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,11 +1,23 @@
 import ManageProducts from "./ManageProducts";
 import SearchBox from "./SearchBox";
+import TableProducts from "./TableProducts";
+import { useState } from "react";
+import Pagination from "./Pagination";
 
 function Dashboard() {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 3;
   return (
     <>
       <SearchBox />
       <ManageProducts />
+      <TableProducts products={products} />
+      {/* <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      /> */}
     </>
   );
 }
---
+++ frontend/src/components/TableProducts.jsx
@@ -0,0 +1,49 @@
+import styles from "./TableProducts.module.css";
+import { FiEdit, FiTrash } from "react-icons/fi";
+
+function TableProducts({ products }) {
+  return (
+    <div className={styles.wrapper}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>نام کالا</th>
+            <th>موجودی</th>
+            <th>قیمت</th>
+            <th> شناسه کالا</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.length === 0 ? (
+            <tr>
+              <td colSpan="5" className={styles.empty}>
+                هیچ محصولی یافت نشد
+              </td>
+            </tr>
+          ) : (
+            products.map((p) => (
+              <tr key={p.id}>
+                <td>{p.name}</td>
+                <td>{p.stock}</td>
+                <td>{p.price}</td>
+                <td>{p.id}</td>
+                <td className={styles.actions}>
+                  <button className={styles.edit}>
+                    <FiEdit />
+                  </button>
+                  <button className={styles.delete}>
+                    <FiTrash />
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TableProducts;
